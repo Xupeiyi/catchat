44d33ae84d66b4903023a95c801b6b7adc3fb102
Peiyi Xu <xu_py311@outlook.com>
2023-06-23 21:13:48 -0400

    created unit test for user and message model

--- catchat/models.py
+++ catchat/models.py
@@ -5,27 +5,46 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User:
 
-    def __init__(self, nickname, email):
-        self.nickname = nickname
+    def __init__(self, email):
+        # self.nickname = nickname
         self.email = email
-        self.password_hash = None
+        # self.password_hash = None
         self.messages = set()
 
-    @property
-    def password(self):
-        raise AttributeError("password is not a readable attribute")
+    def __hash__(self):
+        return hash(self.email)
 
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
+    def __eq__(self, other):
+        if not isinstance(other, User):
+            return False
+        return other.email == self.email
 
-    @staticmethod
-    def generate_email_hash(email):
-        encoded = email.encode('utf-8')
-        return hashlib.md5(encoded).hexdigest()
+    # @property
+    # def password(self):
+    #     raise AttributeError("password is not a readable attribute")
+    #
+    # @password.setter
+    # def password(self, password):
+    #     self.password_hash = generate_password_hash(password)
 
-    def verify_email(self, password):
-        return check_password_hash(self.password_hash, password)
+    # @staticmethod
+    # def generate_email_hash(email):
+    #     encoded = email.encode('utf-8')
+    #     return hashlib.md5(encoded).hexdigest()
+
+    # def verify_password(self, password):
+    #     return check_password_hash(self.password_hash, password)
+
+    def create_message(self, words):
+        message = Message(author=self, body=words)
+        self.messages.add(message)
+        return message
+
+    def delete_message(self, message):
+        if message.author == self:
+            self.messages.remove(message)
+        else:
+            raise ValueError("Can't delete a message of another user!")
 
 
 @dataclass(unsafe_hash=True)
@@ -33,3 +52,4 @@ class Message:
 
     author: User
     body: str
+
---
+++ tests/test_user.py
@@ -0,0 +1,30 @@
+import unittest
+from catchat.models import User
+
+
+class CreateMessageTest(unittest.TestCase):
+    def test_can_retrieve_created_messages(self):
+        bob = User(email='bob@example.com')
+        message1 = bob.create_message('I love pancakes!')
+        self.assertSetEqual(bob.messages, {message1})
+
+    def test_messages_has_correct_creator(self):
+        bob = User(email='bob@example.com')
+        message1 = bob.create_message('I love pancakes!')
+        self.assertEqual(bob, message1.author)
+
+
+class DeleteMessageTest(unittest.TestCase):
+
+    def test_can_delete_ones_own_words(self):
+        bob = User(email='bob@example.com')
+        message1 = bob.create_message('I love pancakes!')
+        message2 = bob.create_message('I love ice-creams!')
+        bob.delete_message(message1)
+        self.assertSetEqual(bob.messages, {message2})
+
+    def test_cannot_delete_others_words(self):
+        bob = User(email='bob@example.com')
+        alice = User(email='alice@example.com')
+        message1 = bob.create_message('I love pancakes!')
+        self.assertRaises(ValueError, alice.delete_message, message1)
